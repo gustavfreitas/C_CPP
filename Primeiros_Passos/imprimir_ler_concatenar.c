3408ace59f58a94e4288927a1df09d18e4c9ff6c
Gustavo Freitas <100533677+gustavfreitas@users.noreply.github.com>
2025-04-08 09:00:07 -0300

    Delete Primeiros_Passos directory

--- Primeiros_Passos/imprimir_ler_concatenar.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-    //Função Principal do Sistema
-void main(){
-
-}
--- Primeiros_Passos/primeiro_teste.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-void main() {
-    //Imprime "oi" na tela
-    printf("oi");
-}
-
