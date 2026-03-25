f09a3da2476cbc7b16f567ab22743c16e6b51a77
Gustavo Freitas <100533677+gustavfreitas@users.noreply.github.com>
2026-03-24 21:13:23 -0300

    Resolução do Exercício 27 e 28

--- Lista03.cpp
+++ Lista03.cpp
@@ -58,3 +58,143 @@ void main() {
 }
 
 */
+
+// Exercício 27
+
+/*
+
+typedef struct No {
+    int valor;
+    No* prox;
+    No* ant;
+} No;
+
+void inserir_inicio(No** head, int v) {
+    No* novo = (No*)malloc(sizeof(No));
+    if (novo == NULL) return;
+    novo->valor = v;
+    novo->ant = NULL;
+    novo->prox = *head;
+
+    if (*head != NULL) {
+        (*head)->ant = NULL;
+    }
+    *head = novo;
+}
+
+void imprime_frente(No* p) {
+    while (p != NULL) {
+        p = p->prox;
+    }
+    printf("NULL\n");
+}
+
+void imprime_traz(No* p) {
+    while (p != NULL) {
+        printf("[%d]->", p->valor);
+        p = p->ant;
+    }
+}
+
+void inserir_fim(No** head, int v) {
+    No* novo = (No*)malloc(sizeof(No));
+    novo->valor = v;
+    novo->prox = NULL;
+
+    if (*head == NULL) {
+        *head = novo;
+        novo->ant = NULL;
+        return;
+    }
+
+    No* p = *head;
+    while (p->prox != NULL) {
+        p = p->prox;
+    }
+    p->prox = novo;
+    novo->ant = p;
+}
+
+void main() {
+    No* head = NULL;
+    inserir_inicio(&head, 10);
+    imprime_traz(head);
+    inserir_inicio(&head, 20);
+    imprime_frente(head);
+    inserir_inicio(&head, 30);
+    imprime_traz(head);
+    inserir_inicio(&head, 40);
+    imprime_frente(head);
+    printf("[NULL]");
+}
+
+*/
+
+// Exercício 28
+
+typedef struct No {
+    int valor;
+    No* prox;
+    No* ant;
+} No;
+
+void inserir_inicio(No** head, int v) {
+    No* novo = (No*)malloc(sizeof(No));
+    if (novo == NULL) return;
+    novo->valor = v;
+    novo->ant = NULL;
+    novo->prox = *head;
+
+    if (*head != NULL) {
+        (*head)->ant = NULL;
+    }
+    *head = novo;
+}
+
+void imprime_frente(No* p) {
+    while (p != NULL) {
+        p = p->prox;
+    }
+    printf("NULL\n");
+}
+
+void imprime_traz(No* p) {
+    while (p != NULL) {
+        printf("[%d]->", p->valor);
+        p = p->ant;
+    }
+}
+
+void inserir_fim(No** head, int v) {
+    No* novo = (No*)malloc(sizeof(No));
+    novo->valor = v;
+    novo->prox = NULL;
+
+    if (*head == NULL) {
+        *head = novo;
+        novo->ant = NULL;
+        return;
+    }
+
+    No* p = *head;
+    while (p->prox != NULL) {
+        p = p->prox;
+    }
+    p->prox = novo;
+    novo->ant = p;
+}
+
+void main() {
+    No* head = NULL;
+    inserir_inicio(&head, 10);
+    imprime_traz(head);
+    inserir_inicio(&head, 20);
+    imprime_frente(head);
+    inserir_inicio(&head, 30);
+    imprime_traz(head);
+    inserir_inicio(&head, 40);
+    imprime_frente(head);
+    inserir_inicio(&head, 50);
+    printf("[NULL]");
+}
+
